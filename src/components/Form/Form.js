7aaee4cc2f9c04dd431ba20b7f2b84aa6789dbbe
Oliinyk Yevgenii <oliinykyevgenii@gmail.com>
2023-05-14 20:20:36 +0300

    custome http hook

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,25 @@
 export const Form = () => {
    return(
        <form action="" className="form">
-           <label htmlFor="name" className="form__title">Input country</label>
-           <input type="text" name="name" placeholder="Ukraine" className="form__input" autocomplite="off"/>
+           <label htmlFor="name" className="form__title">Choose the country in which you will study</label>
+           <input
+               type="text"
+               name="name"
+               placeholder="Ukraine"
+               className="form__input"
+               autocomplite="off"
+           />
            <div className="form__action">
-               <button type="submit" className="btn form__submit">Submit</button>
-               <button type="reset" className="btn form__reset">Reset</button>
+               <button
+                   type="submit"
+                   className="btn form__submit"
+               >Submit</button>
+               <button
+                   type="reset"
+                   className="btn form__reset"
+               >Reset</button>
            </div>
        </form>
+
    )
 }
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,11 +1,23 @@
+import { useState, useCallback } from "react";
+
 import { Form } from "../../components/Form/Form";
 
 const MainPage = () => {
-   return(
-       <main>
-           <Form/>
-       </main>
-   )
+
+
+    return (
+        <main>
+            <Form
+               
+            />
+            <button
+                type="submit"
+                className="btn more"
+            >
+                Load more
+            </button>
+        </main>
+    )
 }
 
 export default MainPage;
