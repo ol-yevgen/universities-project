92033fb41eba8d0fd7a806c291f57c9ce8c5e84c
Oliinyk Yevgenii <oliinykyevgenii@gmail.com>
2023-05-14 20:29:29 +0300

    create state

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -3,7 +3,11 @@ import { useState, useCallback } from "react";
 import { Form } from "../../components/Form/Form";
 
 const MainPage = () => {
-
+    const [country, setCountry] = useState('');
+    const [universityList, setUniversitylist] = useState([]);
+    const [offset, setOffset] = useState(10);
+    const [universityEnded, setUniversityListEnded] = useState(false);
+    
     return (
         <main>
             <Form
